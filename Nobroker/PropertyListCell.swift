9544b2b2032ca791d21870c4a6b41197682d89b4
Sanjay Mali <sanjaymali@Sanjays-MacBook-Pro.local>
2017-04-30 01:38:39 +0530

    initial commit of nobroker design

---
+++ Nobroker/PropertyListCell.swift
@@ -0,0 +1,24 @@
+//
+//  PropertyListCell.swift
+//  Nobroker
+//
+//  Created by Sanjay Mali on 29/04/17.
+//  Copyright © 2017 Sanjay Mali. All rights reserved.
+//
+
+import UIKit
+
+class PropertyListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
